dfe3d099e2a7a8dc91e6c66f7cd08380d24d64b4
Benjamin Nomba <bnomba0704@gmail.com>
2023-03-27 07:17:34 -0700

    about

--- src/components/MyAboutMe.vue
+++ src/components/MyAboutMe.vue
@@ -2,13 +2,11 @@
 	<div id="about" class="About flex h-screen justify-center items-center gap-12">
 		<div class="about-me w-96 text-left">
 			<h1>About</h1>
-			Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
+			Hello! My name is Benjamin and I enjoy creating things that live on the internet. My interest in web development and engineering started back in 2018 when I started my computer science major — I started with the basics and soon after I knew a lot about HTML & CSS!
 
-			Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.
+			Fast-forward to today, and I’ve had the privilege of attending Alt-School Africa. A Pan-African Technology School and a huge organisation. My main focus these days is building accessible, inclusive products and digital experiences for a variety of users.
 
-			I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.
-
-			Here are a few technologies I’ve been working with recently
+			I also Do Technical writting and I like to learn various technologies at my spare time which will help me build better web applictions.
 		</div>
 
 		<div class="image w-72 rounded-full">
